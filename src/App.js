c9319b3c92e788772eedd8dc2db8f19dff739098
codeinsack <codeinsack@gmail.com>
2022-03-16 20:18:05 +0300

    finish

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import { ThemeProvider } from "styled-components";
-import { Container } from "./components/styles/Container.styled";
-import Header from "./components/Header";
-import GlobalStyles from "./components/styles/Global";
+import { ThemeProvider } from 'styled-components'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Card from './components/Card'
+import { Container } from './components/styles/Container.styled'
+import GlobalStyles from './components/styles/Global'
+import content from './content'
 
 const theme = {
   colors: {
-    header: "#ebfbff",
-    body: "#fff",
-    footer: "#003333",
+    header: '#ebfbff',
+    body: '#fff',
+    footer: '#003333',
   },
-};
+  mobile: '768px',
+}
 
 function App() {
   return (
@@ -18,11 +22,14 @@ function App() {
         <GlobalStyles />
         <Header />
         <Container>
-          <h1>Styled Components</h1>
+          {content.map((item, index) => (
+            <Card key={index} item={item} />
+          ))}
         </Container>
+        <Footer />
       </>
     </ThemeProvider>
-  );
+  )
 }
 
-export default App;
+export default App
