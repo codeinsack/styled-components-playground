35e160c834d8bfec18e6d9301aa128c05f957fd5
codeinsack <codeinsack@gmail.com>
2022-03-16 20:09:43 +0300

    add basic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import "./App.css";
+import { ThemeProvider } from "styled-components";
+import { Container } from "./components/styles/Container.styled";
+import Header from "./components/Header";
+import GlobalStyles from "./components/styles/Global";
+
+const theme = {
+  colors: {
+    header: "#ebfbff",
+    body: "#fff",
+    footer: "#003333",
+  },
+};
 
 function App() {
   return (
-    <div>
-      <h1>Styled Components</h1>
-    </div>
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyles />
+        <Header />
+        <Container>
+          <h1>Styled Components</h1>
+        </Container>
+      </>
+    </ThemeProvider>
   );
 }
 
